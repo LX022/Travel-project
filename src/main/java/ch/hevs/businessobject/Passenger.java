f16e5f93cdf72a38589f407be3c815fdb9c21242
Borganov <berclazalexandre@gmail.com>
2019-04-19 10:01:40 +0200

    Update Pssengers.java

--- src/main/java/ch/hevs/businessobject/Passenger.java
+++ src/main/java/ch/hevs/businessobject/Passenger.java
@@ -1,10 +1,15 @@
 package ch.hevs.businessobject;
 
-import javax.persistence.Column;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -14,10 +19,74 @@ public class Passenger {
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	private Long id;
-	@Column(name="numero")
-	private String number;
-	@Column(name="solde")
-	private long saldo;	
-	private String description;
+	private String firstname;
+	private String lastname;	
+	private String address;
+	private String zipcode;
+
+	// relations
+	@ManyToMany/*(mappedBy="passengers")*/
+	private List<Flight> flights;
+
+	
+	// CONSTRUCTORS
+	public Passenger() {
+		
+	}
+	
+	public Passenger(Long id, String firstname, String lastname, String address, String zipcode) {
+		super();
+		this.id = id;
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.address = address;
+		this.zipcode = zipcode;
+	}
+
+
+	// GETTERS AND SETTERS
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getZipcode() {
+		return zipcode;
+	}
+
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+	
+
+	
+	
 
 }
