21556505955a167a181fa8e0d860cd9e400ba841
Yann <yann.cla@gmail.com>
2019-04-19 09:48:55 +0200

    Create Destination entity

---
+++ src/main/java/ch/hevs/businessobject/Destination.java
@@ -0,0 +1,46 @@
+package ch.hevs.businessobject;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Destination")
+public class Destination {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private long id;
+	private String city;
+	private String country;
+	private String airport;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getAirport() {
+		return airport;
+	}
+	public void setAirport(String airport) {
+		this.airport = airport;
+	}
+	
+	
+
+}
