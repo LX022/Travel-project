8423370cf35a7475a3067ecf9c6283447a26866d
Yann <yann.cla@gmail.com>
2019-04-19 09:44:59 +0200

    Update Flight.java
    Ajout variables

--- src/main/java/ch/hevs/businessobject/Flight.java
+++ src/main/java/ch/hevs/businessobject/Flight.java
@@ -1,5 +1,8 @@
 package ch.hevs.businessobject;
 
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,5 +15,9 @@ public class Flight {
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	private long id;
+	private String flightNumber;
+	private Date date;
+	private String aircraftModel;
+	private long price;
 
 }
